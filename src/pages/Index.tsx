bb7617eb9d9677fdadd2deb563f50eff7652f8ab
Citrine Global India <citrineglobalindia@gmail.com>
2026-07-11 13:14:15 +0530

    Homepage: remove Residences ('A new vocabulary of living'), The Standard, and In the Press sections

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,10 +3,7 @@ import Nav from "@/components/ui/Nav";
 import Footer from "@/components/ui/Footer";
 import Hero from "@/components/sections/Hero";
 import Collection from "@/components/sections/Collection";
-import Residences from "@/components/sections/Residences";
-import Standard from "@/components/sections/Standard";
 import WhyCity from "@/components/sections/WhyCity";
-import Press from "@/components/sections/Press";
 import News from "@/components/sections/News";
 import CTA from "@/components/sections/CTA";
 
@@ -21,10 +18,7 @@ const Index = () => {
       <Nav />
       <Hero />
       <Collection />
-      <Residences />
-      <Standard />
       <WhyCity />
-      <Press />
       <News />
       <CTA />
       <Footer />
